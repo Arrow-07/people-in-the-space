a25332518b674d568fdc512203153ff74437c0fb
Arrow07 <100967928+Arrow-07@users.noreply.github.com>
2025-01-03 18:03:13 +0100

    Update info.py

--- info.py
+++ info.py
@@ -2,8 +2,8 @@ from pathlib import Path
 from tkinter import Toplevel, Canvas, Text, PhotoImage
 import webbrowser
 
-OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\giaco\OneDrive\Documenti\Desktop\PRESEPEVIVENTE\build\assets\frame0")
+OUTPUT_PATH = Path(__file__).parent  # Questo punta alla cartella "build"
+ASSETS_PATH = OUTPUT_PATH / "assets" / "frame0"  # Percorso relativo al file
 
 
 def relative_to_assets(path: str) -> Path:
